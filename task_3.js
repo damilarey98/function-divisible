06d72068ece570f2aca894f5b31cba85686a3116
Sodiq Akintunde <damilarey98@gmail.com>
2020-04-18 09:45:48 +0100

    new file added

---
+++ task_3.js
@@ -0,0 +1,30 @@
+function divisible(arr) {
+    let data = []
+    for (i = 1; i <=arr; i++){
+        if((i%2===0) && (i%3===0) && (i%5===0)){
+            data.push(console.log("yu-gi-oh"));
+        }
+        else if((i%2===0) && (i%3===0) && (i%5!==0)){
+            data.push(console.log("yu-gi"));
+        }
+        else if((i%2===0) && (i%3!==0) && (i%5===0)){
+            data.push(console.log("yu-oh"));
+        }
+        else if((i%2!==0) && (i%3===0) && (i%5===0)){
+            data.push(console.log("gi-oh"));
+        }
+        else if((i%2===0) && (i%3!==0) && (i%5!==0)){
+            data.push(console.log("yu"));
+        }
+        else if((i%2!==0) && (i%3===0) && (i%5!==0)){
+            data.push(console.log("gi"));
+        }
+        else if((i%2!==0) && (i%3!==0) && (i%5===0)){
+            data.push(console.log("oh"));
+        }
+        else {
+            data.push(console.log(i))
+        }
+    }
+}
+divisible(10)
